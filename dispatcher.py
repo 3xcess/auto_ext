831b34e21acdb8c77601372b79ac8440afe85af3
Eddie Federmeyer <eddiefedermeyer@gmail.com>
2025-05-08 19:51:08 -0500

    Dispatcher
    + dispatcher now dynamically loads the scheduler!
    + Moved eval location

--- dispatcher.py
+++ dispatcher.py
@@ -3,27 +3,43 @@ import ctypes
 import subprocess
 import time
 
+from eval import evaluate, SystemLoad
+
+# TODO: For now each workload is a single scheduler, ideally we could have overlapping workloads do something else too
+SCHED_PATH = "/home/eddie/scx/build/scheds/c"
+scheds = {
+    SystemLoad.CPU:     "scx_simple", 
+    SystemLoad.IO:      "scx_prev", 
+    SystemLoad.MEM:     "scx_simple", 
+    SystemLoad.NET:     "scx_nest",
+    SystemLoad.IDLE:    "scx_simple"
+}
+load = { }
+for s_load in list(scheds.keys())[:-1]:
+    load[s_load] = False
+
+curr_load = SystemLoad.CPU
+
 SLEEP_INTERVAL = 2
 THRESHOLD = 500
 
-# TODO: Init automatically
-load = {"CPU": False, "IO": False, "MEM": False, "NET": False}
-
-bpf_source = f"""
-BPF_TABLE_PINNED("hash", u64, u64, ba_bawm, 1024, "/sys/fs/bpf/ba_bawm");        
-"""
-
+p = subprocess.Popen([f'{SCHED_PATH}/{scheds[SystemLoad.CPU]}'], stdout=subprocess.DEVNULL)
 while(True):
-    b = BPF(text=bpf_source)
+    b = BPF(text='BPF_TABLE_PINNED("hash", u64, u64, ba_bawm, 1024, "/sys/fs/bpf/ba_bawm");')
     
-    for i in range(4):
-        # TODO: This is fucked man, make an enum
-        load[list(load.keys())[i]] = True if b["ba_bawm"].get(ctypes.c_uint(i)).value >= THRESHOLD else False
-        print(b["ba_bawm"].get(ctypes.c_uint(i)).value)
+    # TODO: Move this logic to the individual ebpf programs
+    # This should also include making a bool value to mark if the load is high or not
+    for i, s_load in enumerate(load): 
+        load[s_load] = True if b["ba_bawm"].get(ctypes.c_uint(i)).value >= THRESHOLD else False
 
     print(load)
 
-
+    new_load = evaluate(*list(load.values()))
+    if not (curr_load == new_load and load[new_load] == True):
+        curr_load = new_load
+        p.kill()
+        p = subprocess.Popen([f'{SCHED_PATH}/{scheds[curr_load]}'], stdout=subprocess.DEVNULL)
+        print(f"Switched to {scheds[curr_load]}, {curr_load.name}")
 
     b["ba_bawm"].clear()
     time.sleep(SLEEP_INTERVAL)
---
+++ eval.py
@@ -0,0 +1,23 @@
+
+import enum
+
+class SystemLoad(enum.Enum):
+    CPU     = 0
+    IO      = 1
+    MEM     = 2
+    NET     = 3
+    IDLE    = 5
+
+def evaluate(cpu, io, mem, network):
+    
+    if network:
+        return SystemLoad.NET
+    else:
+        if io:
+            return SystemLoad.IO
+        elif cpu:
+            return SystemLoad.CPU
+        elif mem:
+            return SystemLoad.MEM
+
+    return SystemLoad.IDLE
